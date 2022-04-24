202d4635ed96fbc78ef544539676c8953c81e22b
Abhiraj Ghosh <21bca1058@cuchd.in>
2022-04-25 03:09:55 +0530

    initial commit

---
+++ src/useFetch.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-unused-vars */
+import { useState, useEffect,  } from "react";
+
+ // eslint-disable-next-line no-unused-vars
+ 
+
+const useFetch = (url) => {
+
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+
+    useEffect(() => {
+
+        setTimeout(()=> {
+            fetch(url, ).then(res => {
+                if (!res.ok){
+                    throw Error('could not fetch the data for that resource.')
+                }
+            return  res.json()
+        }).then((data) => {
+            setData(data)
+            setIsPending(false)
+            setError(null)
+        }).catch(err => {
+            if ( err.name === 'AbortController'){
+                console.log('fetch aborted')
+            } else {
+                setIsPending(false)
+                setError(err.message);
+            }
+        })
+        }, 1000)
+    }, [url]);
+
+    return {
+        data,
+        isPending,
+        error
+    }
+}
+
+export default useFetch;
